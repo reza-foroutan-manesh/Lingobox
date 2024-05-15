3646f2b4b537df771977d532c331e352c207ad0d
Reza Foroutan <MrHumble10>
2024-05-15 17:46:17 -0300

    modify menu and page links

--- main.py
+++ main.py
@@ -165,5 +165,15 @@ def dictionary():
     return render_template('dict.html', languages=native_names)
 
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
 if __name__ == "__main__":
     app.run(debug=True, port=5004)
